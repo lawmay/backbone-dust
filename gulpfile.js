6d94c00005cd825ce8a3e9464cff4fc16a66d53e
LM <lmay01@gmail.com>
2014-11-12 19:18:19 -0800

    concat dust templates

--- gulpfile.js
+++ gulpfile.js
@@ -4,19 +4,30 @@ var source = require('vinyl-source-stream');
 var uglify = require('gulp-uglify');
 var streamify = require('gulp-streamify');
 var dust = require('gulp-dust');
+var concat = require('gulp-concat');
 
 var paths = {
-  src: ['src/**/*.js'],
-  templates: ['templates/*.dust']
+  src: ['src/**/*.js']
+  , templates: ['templates/*.dust']
+  , compiledTemplates: ['dist/templates/*.js']
   , styles: ['styles/styles.css']
 };
 
-gulp.task('templates', function () {
+gulp.task('compileTemplates', function () {
   return gulp.src(paths.templates)
     .pipe(dust())
     .pipe(gulp.dest('dist/templates'))
   ;
 });
+
+gulp.task('concatTemplates', function () {
+  return gulp.src(paths.compiledTemplates)
+    .pipe(concat('dust-templates.js'))
+    .pipe(gulp.dest('dist'))
+  ;
+});
+
+gulp.task('templates', ['compileTemplates', 'concatTemplates']);
 
 gulp.task('browserify', function() {
   return browserify('./src/main.js')
